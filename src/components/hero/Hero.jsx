81cb3e360dbd331dfbbec40e82a6f19eeb8cfc02
marceelp <marcelpedaa@googlemail.com>
2023-06-06 22:09:50 +0200

    move main menu and sections up + center everything

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,21 +9,19 @@ const Hero = ({ activeLink }) => {
       id="hero__section"
       className={activeLink === "Home" ? "visible" : "invisible"}
     >
-      <article id="content">
-        <div id="description">
-          <h1>{heading}</h1>
-          <p>
-            Mein Name ist Marcel Peda.
-            <br />
-            <br />
-            Ich bin ein 26 Jahre junger, leidenschaftlicher Full-Stack-Developer
-            aus Hamburg, Deutschland.
-            <br />
-            <br />
-            Ich bin daran interessiert, moderne User Interfaces und intuitiv
-            gestaltete User Experiences zu erschaffen.
-          </p>
-        </div>
+      <article id="description">
+        <h1>{heading}</h1>
+        <p>
+          Mein Name ist Marcel Peda.
+          <br />
+          <br />
+          Ich bin ein 26 Jahre junger, leidenschaftlicher Full-Stack-Developer
+          aus Hamburg, Deutschland.
+          <br />
+          <br />
+          Ich bin daran interessiert, moderne User Interfaces und intuitiv
+          gestaltete User Experiences zu erschaffen.
+        </p>
       </article>
     </section>
   );
