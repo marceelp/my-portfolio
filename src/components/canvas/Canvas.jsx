76dc06871feef0770be4e7cf1447dcb042a4aa67
marceelp <marcelpedaa@googlemail.com>
2023-06-09 23:35:25 +0200

    change: colorTheme/gradients

--- src/components/canvas/Canvas.jsx
+++ src/components/canvas/Canvas.jsx
@@ -7,11 +7,11 @@ class CanvasBackground extends Component {
     canvas.height = innerHeight;
     const c = canvas.getContext("2d");
 
-    let circles = [];
-    const colors = ["#FF4858", "#1B7F79", "#00CCC0", "#72F2EB"];
     const mouse = { x: undefined, y: undefined };
+    const colors = ["#FF4858", "#1B7F79", "#00CCC0", "#72F2EB"];
+    const circles = [];
 
-    const randomInt = (min, max) => {
+    const randomIntFromRange = (min, max) => {
       return Math.floor(Math.random() * (max - min + 1) + min);
     };
 
@@ -19,11 +19,11 @@ class CanvasBackground extends Component {
       constructor() {
         this.x = mouse.x;
         this.y = mouse.y;
-        this.radius = randomInt(5, 10);
-        this.color = colors[Math.floor(Math.random() * colors.length)];
+        this.radius = randomIntFromRange(5, 10);
+        this.color = colors[Math.floor(Math.random() * 4)];
         this.speed = {
-          x: randomInt(-2, 2),
-          y: randomInt(-2, 2),
+          x: randomIntFromRange(-2, 2),
+          y: randomIntFromRange(-2, 2),
         };
       }
 
@@ -36,12 +36,12 @@ class CanvasBackground extends Component {
       }
 
       update() {
+        this.draw();
+
         this.x += this.speed.x;
         this.y += this.speed.y;
 
         if (this.radius > 0.2) this.radius -= 0.07;
-
-        this.draw();
       }
     }
 
@@ -49,9 +49,7 @@ class CanvasBackground extends Component {
       mouse.x = e.clientX;
       mouse.y = e.clientY;
 
-      for (let i = 0; i < 1; i++) {
-        circles.push(new Circle());
-      }
+      circles.push(new Circle());
     });
 
     const animate = () => {
@@ -59,42 +57,34 @@ class CanvasBackground extends Component {
       c.fillStyle = "#131313";
       c.fillRect(0, 0, canvas.width, canvas.height);
 
-      for (let i = 0; i < circles.length; i++) {
-        circles[i].update();
+      circles.forEach((circle, i) => {
+        circle.update();
 
-        for (let j = 0; j < circles.length; j++) {
-          const dx = circles[i].x - circles[j].x;
-          const dy = circles[i].y - circles[j].y;
+        circles.forEach((otherCircle, j) => {
+          const dx = circle.x - otherCircle.x;
+          const dy = circle.y - otherCircle.y;
           const distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 
           if (distance < 100) {
             c.beginPath();
-            c.moveTo(circles[i].x, circles[i].y);
-            c.lineTo(circles[j].x, circles[j].y);
+            c.moveTo(circle.x, circle.y);
+            c.lineTo(otherCircle.x, otherCircle.y);
             c.lineWidth = 0.1;
-            c.strokeStyle = circles[i].color;
+            c.strokeStyle = circle.color;
             c.stroke();
             c.closePath();
           }
-        }
+        });
 
-        if (circles[i].radius < 2) {
-          circles.splice(i, 1);
-        }
-      }
+        if (circle.radius < 2) circles.splice(i, 1);
+      });
     };
+
     animate();
   }
 
   render() {
-    return (
-      <div>
-        <canvas
-          ref={(canvas) => (this.canvas = canvas)}
-          style={{ margin: "auto 0" }}
-        ></canvas>
-      </div>
-    );
+    return <canvas ref={(canvas) => (this.canvas = canvas)}></canvas>;
   }
 }
 
