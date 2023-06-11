6af4ea80e02e2e18c30863905a418c19dbc7842a
marceelp <marcelpedaa@googlemail.com>
2023-06-11 19:53:38 +0200

    rm: mobile red background

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
         </Routes>
         <Nav />
         <Frame />
-        <CanvasBackground />
+        {/* <CanvasBackground /> */}
       </div>
     </PageProvider>
   );
