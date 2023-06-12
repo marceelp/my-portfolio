7d0d68ef5f07db2b4ad23465f00168aac2f67d35
marceelp <marcelpedaa@googlemail.com>
2023-06-12 23:58:53 +0200

    fix: mobile menu btn functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { PageProvider } from "./contexts/PageContext";
 
@@ -9,44 +8,24 @@ import Skills from "./components/skills/Skills";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Nav from "./components/nav/Nav";
-import MobileNav from "./components/mobileNav/MobileNav";
 import Frame from "./components/frame/Frame";
 import CanvasBackground from "./components/canvas/Canvas";
 
 const App = () => {
-  const [mobile, setMobile] = useState(false);
   const location = useLocation();
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 768) {
-        setMobile(true);
-      } else {
-        setMobile(false);
-      }
-    };
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <PageProvider>
       <main className="wrapper">
         <Preloader />
-        <Header mobile={mobile} />
+        <Header />
         <Routes location={location}>
           <Route path="/my-portfolio" element={<Home />} />
           <Route path="/my-portfolio/skills" element={<Skills />} />
           <Route path="/my-portfolio/projekte" element={<Projects />} />
           <Route path="/my-portfolio/kontakt" element={<Contact />} />
         </Routes>
-        {mobile ? <MobileNav /> : <Nav />}
+        <Nav />
         <Frame />
         <CanvasBackground />
       </main>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,37 +1,64 @@
+import { useEffect, useState } from "react";
+import LOGO from "../../assets/images/logo.png";
+import MobileMenuBtn from "./MobileMenuBtn";
 import { FaGithub, FaInstagram } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
-import LOGO from "../../assets/images/logo.png";
 import "./header.css";
 
-function Header({ mobile }) {
+function Header() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [mobile, setMobile] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (innerWidth <= 768) setMobile(true);
+      else setMobile(false);
+    };
+    handleResize();
+
+    addEventListener("resize", handleResize);
+    return () => removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <header>
-      <img src={LOGO} alt="Marcel Peda Logo" className="nav--logo" />
-      <div>
+      <img src={LOGO} alt="Marcel Peda Logo" className="header--logo" />
+
+      <div className="header--icons">
         {!mobile && (
           <a
             href="mailto:marcelpedaa@googlemail.com"
             alt="Mail an Marcel Peda"
             target="_blank"
           >
-            <SiGmail className="nav--icon nav--icon-one" />
+            <SiGmail className="header--icon one" />
           </a>
         )}
+
         <a
           href="https://github.com/marceelp"
           alt="Github Marcel Peda"
           target="_blank"
         >
-          <FaGithub className="nav--icon nav--icon-two" />
+          <FaGithub className="header--icon two" />
         </a>
+
         {!mobile && (
           <a
             href="https://instagram.com/marceelp"
             alt="Instagram Marcel Peda"
             target="_blank"
           >
-            <FaInstagram className="nav--icon nav--icon-three" />
+            <FaInstagram className="header--icon three" />
           </a>
+        )}
+
+        {mobile && (
+          <MobileMenuBtn isClicked={isClicked} handleClick={handleClick} />
         )}
       </div>
     </header>
